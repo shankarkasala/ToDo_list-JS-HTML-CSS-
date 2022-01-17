88ec64efd20baee042ae4416e010b2c5660140d2
shankarkasala <umashankarno66@gmail.com>
2022-01-17 22:43:26 +0530

    implimenting select feature

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //eventlistener
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
-
+filterOption.addEventListener("click", filterTodo);
 //functions
 
 function addTodo(event) {
@@ -36,11 +37,33 @@ function deleteCheck(e) {
   const item = e.target;
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
 
   if (item.classList[0] === "complete-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  console.log(todos);
+  toxos.forEach(function () {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+      default:
+        break;
+    }
+  });
 }
